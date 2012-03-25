d6a28a33177d2286b36025386dcac5cd4422029a
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2012-03-25 17:06:26 +0100

    Updated instaedit to work with new project_data module

--- bin/instaedit.py
+++ bin/instaedit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import sys
-import command
+import project_data
 
-current_project = "/home/jon/public_html/data/projectID4ed1b63667527/project"
-d = command.ProjectData(current_project)
+current_project = "/home/jon/public_html/data/projectID4f680ded3a815/project"
+d = project_data.ProjectData(current_project)
 pageid = open(sys.argv[1]).read(3)
-text = d.get_text(pageid, "127.0.0.1")
+text = unicode(d.get_text(pageid, "127.0.0.1")[0], "utf-8")
 open(sys.argv[1], "w").write(text.encode("utf-8"))
