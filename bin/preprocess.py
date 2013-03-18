19adbc79285db9f451ef89997f5a9132503462ed
Jon Hurst <jon.a@hursts.eclispe.co.uk>
2013-03-18 08:46:02 +0000

    Modify preproof for latest version of tesseract

--- bin/preprocess.py
+++ bin/preprocess.py
@@ -8,19 +8,28 @@ import sys
 pd = project_data.ProjectData(sys.argv[1], True)
 pages = pd.get_pages()
 for p in pages:
+    if pd.exists(p[0], "preproof"): continue
+    text, timestamp, status = pd.get_text(p[0], "ocr")
     print p[0]
-    if int(p[0]) < 57: continue
-    text, timestamp, status = pd.get_text(p[0], "127.0.0.1")
+    text = text.replace("ﬁ", "fi")
+    text = text.replace("ﬀ", "ff")
+    text = text.replace("ﬂ", "fl")
+    text = re.sub(r"\n+", "\n", text)
     text = re.sub(r"‘\s*", "'", text)
     text = re.sub(r"\s*’", "'", text)
+    text = re.sub(r"“\s*", '"', text)
+    text = re.sub(r"\s*”", '"', text)
     text = text.replace("`", "'")
-    text = text.replace("—", "-")
+    text = text.replace("—", "--")
     text = text.replace("]", "J")
     text = text.replace("·", "")
+    text = text.replace("VV", "W")
+    text = text.replace("\V", "W")
+    text = re.sub(r" *--+ *", "--", text)
     text = re.sub(r"\s*([\;\:\!\?\,])", r"\1", text)
     text = re.sub(r"(\n[\'\"])\s*", r"\1", text)
     text = re.sub(r"\s*([\'\"]\n)", r"\1", text)
-    pd.set_text(p[0], text, "127.0.0.1", False)
+    pd.set_text(p[0], text, "ocr", False)
 pd.save()
 pd.unlock()
 
